490d496681001f098aab65b42e3a953acd64e517
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-09-27 19:24:06 +0200

    Add possibility to create a new bookmark (= add a movie to a list)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, except: [:destroy]
+  resources :lists, except: [:destroy] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
