7da9b72d5082d53d4a1090188ce5ee881894a6c2
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-09-27 17:56:07 +0200

    Create all the models validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
-  belongs_to :bookmark
+  has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_27_144852) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_27_154854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
     t.bigint "movie_id", null: false
     t.bigint "list_id", null: false
     t.datetime "created_at", null: false
